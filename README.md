5ce668b9ce4a5094c18c021ebf037bac45d9848f
KimMinJoon <kimminjoooon@gmail.com>
2021-12-16 15:18:54 +0900

    2021-12-16 start

--- README.md
+++ README.md
@@ -1,20 +1,17 @@
+
 ### 코딩테스트 준비하기
+
 1. 백준알고리즘 
 [https://www.acmicpc.net/step/1]
 
 2. leetcode
 [https://leetcode.com/]
 
-The Flea experiment
-
-I'm not Flea.
-
-but I need fire under my feet.
-
-beyond the jar.
+3. programmers
+[https://programmers.co.kr/]
 
 
-3. C++ 로 코딩테스트를 준비한다. 
+4. C++ 로 코딩테스트를 준비한다. 
 - stl 라이브러리 사용에 좋고
 - 코드 최적화 메모리 접근가능
 - 내가 원하는 언어 
@@ -24,7 +21,6 @@ windows 와 vsCode에서 c++ 컴파일을 위한 절차
 2. make 컴파일러 설치 
 3. makefile 생성
 
-빌드 환경을 구성해야 된다.
 
 
 
--- ongoing/ongoing.cpp
+++ ongoing/ongoing.cpp
@@ -5,43 +5,56 @@
 using namespace std;
 
 /**
- * 백준알고리즘 문제 연습장
+ * https://programmers.co.kr/learn/courses/30/lessons/42576
  * 
  **/
 
-void divideNumPosition(int num, int arry[], int size);
+string solution(vector<string> participant, vector<string> completion);
 
 int main() {
-    int a = 0;
-    int b = 0;
-    int res1, res2, res3, res4 = 0;
 
-    int arry_a[] = { 0, 0, 0 };
-    int arry_b[] = { 0, 0, 0 };
+    string answer = "";
+    // 1. ["leo", "kiki", "eden"], ["eden", "kiki"]
+    // 2. ["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]
+    vector<string> participant = {"marina", "josipa", "nikola", "vinko", "filipa"};
+    vector<string> completion= {"josipa", "filipa", "marina", "nikola"};
 
-    std::cin >> a ;
-    std::cin >> b ;
+    answer.assign(solution(participant, completion));
 
-    divideNumPosition(b, arry_b, sizeof(arry_b));
+    std::cout << "answer :" << answer << endl;
 
-    res1 = a * arry_b[2];
-    std::cout << res1 << std::endl;
-
-    res2 = (a * arry_b[1]);
-    std::cout << res2 << std::endl;
-
-    res3 = (a * arry_b[0]);
-    std::cout << res3 << std::endl;
-
-    res4 = res1 + res2*10 + res3*100;
-    std::cout << res4 << std::endl;
-
-    // system("pause");
+    system("pause");
     return 0;
 }
 
-void divideNumPosition(int num, int arry[], int size){
-    arry[0] = num / 100;
-    arry[1] = (num - (arry[0] * 100) ) / 10;
-    arry[2] = (num - ((arry[0] * 100) + (arry[1] * 10)));
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+
+    vector<string> temp = participant;
+
+    for (size_t i = 0; i < participant.size(); i++)
+    {
+        for (size_t j = 0; j < completion.size(); j++)
+        {
+            if (participant[i].compare(completion[j]) == 0) {
+                std::cout << "participant :" << participant[i] << ", "<< "completion : "<<  completion[j] << endl;
+                
+                // std::cout << "temp.begin()+i  :: " << temp.begin() << std::endl;
+                temp.erase(temp.begin() + i);
+                continue;
+            }
+        }
+    }
+    
+    for (size_t i = 0; i < temp.size(); i++)
+    {
+        std::cout << "participant : " << temp[i]   << std::endl;
+    }
+    
+    // participant.resize(1);
+    // answer.assign(participant[0]);
+
+    
+    return answer;
 }
+
