9507eb3b5d2e81135df330be1a94e259ab6a5996
KimMinJoon <kimminjoooon@gmail.com>
2021-12-22 17:27:42 +0900

    베스트앨범 코테진행중

--- ongoing/java/ongoing/src/App.java
+++ ongoing/java/ongoing/src/App.java
@@ -4,10 +4,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         Solution sol = new Solution(); 
 
-        int answer;
+        
+        int[] answer;
+
+        // int answer;
         // String[][] clothes = {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}};
-        String[][] clothes = {{"crowmask", "face"}, {"bluesunglasses", "face"}, {"smoky_makeup", "face"}};
-        answer = sol.solution1(clothes);
+        // String[][] clothes = {{"crowmask", "face"}, {"bluesunglasses", "face"}, {"smoky_makeup", "face"}};
+        // answer = sol.solution1(clothes);
+
+
+        // ["classic", "pop", "classic", "classic", "pop"]	[500, 600, 150, 800, 2500]	
+        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
+        int[] plays = {500, 600, 150, 800, 2500};
+        answer = sol.solution2(genres, plays);
 
         System.out.println("return : " + answer);
     }
--- ongoing/java/ongoing/src/Solution.java
+++ ongoing/java/ongoing/src/Solution.java
@@ -1,6 +1,8 @@
 package ongoing.java.ongoing.src;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
 
@@ -13,6 +15,8 @@ class Solution {
      * @param clothes
      * @return int
      */
+
+
     public int solution1(String[][] clothes) {
         int answer = 1;
 
@@ -36,6 +40,46 @@ class Solution {
         // 3. 전부다 안입는 경우 1을 빼주고 리턴.
         return answer - 1; 
     }
+
+
+
+    /**
+     * 해시문제 - "베스트앨범"
+     * https://programmers.co.kr/learn/courses/30/lessons/42579
+     * 
+     * @param clothes
+     * @return int
+     */
+
+    public int[] solution2(String[] genres, int[] plays) {
+        int[] answer = {};
+
+        // 1. 장르의 종류를 구함
+        Set<String> genreSet = new HashSet<>();
+        for(String str : genres) {
+            genreSet.add(str);      // 중복제거
+        }
+
+        for( ) {
+
+        }
+
+        // 2. 장르별 조회수가 높은 2곡을 조회 및 저장
+        // 3. 장르별 가장 조회수가 높은 곡들을 나열 
+        
+
+
+
+
+
+        
+        // 1. 장르별에서 조회수 높은 것들을 가져와 비교 -> 조회수가 가장높은 곡을 포함한 장르순서 저장
+        // 2. 각 장르별 조회수 높은 2곡을 순서대로 저장. 조회수가 같을 경우 고유번호로 나열.
+
+        return answer;
+    }
+
+    
 
     /***************************************************************************/
 
