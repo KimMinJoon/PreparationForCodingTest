1dbc784750bab4a29deea15e7b6bf399eb362dbe
KimMinJoon <kimminjoooon@gmail.com>
2022-04-28 18:13:16 +0900

    다리를 지나는 트럭 진행 중

--- ongoing/java/ongoing/src/Solution.java
+++ ongoing/java/ongoing/src/Solution.java
@@ -37,43 +37,58 @@ class Solution {
 
         int seconds = 0 ;
         int sumWeight = 0;
+
+        Queue<Integer> waitTruck = new LinkedList<>();
         Queue<Integer> bridge = new LinkedList<>();
+
+        // 대기 큐에 입력
+        for (int i = 0; i < truck_weights.length; i++) {
+            waitTruck.add(truck_weights[i]);
+        }
         
 
         // 트럭 IN  / 트럭 OUT / 트럭 MOVE 이 3가지가 동시에 1초에 진행되어야함.  
         // 트럭 1개가 무게가 많이 나가서 다리를 오랫동안 점유할 경우 카운팅이 여러번 되어야하기때문에 while문 활용
         
-        while(1) {
+        
+        while(waitTruck.size() != 0 && bridge.size() != 0) {            // 대기와 다리에 트럭이 없으면 종료.
 
-            for (int i = 0; i < truck_weights.length; i++) {
-                // 7, 4, 5, 6
-    
-                // 다리 입장 가능 차량 수 확인조건
-                if (bridge.size() < bridge_length) {
-    
-                    // 다리 입장 가능 총 중량 확인 조건
-                    if (sumWeight + truck_weights[i]< weight) {
-    
-                        bridge.add(truck_weights[i]);
-    
-                        sumWeight += truck_weights[i];
-                        seconds++;
-                    } else {
-                        bridge.add(0);      // 중량 초과로 0인 무게의 트럭을 큐에 추가.???
-                    }
-    
+            int enterTruck = waitTruck.peek();      // 대기중인 맨앞의 트럭을 가져온다. 
+            // 7, 4, 5, 6
+            // 다리 입장 가능 차량 수 확인조건
+            if (bridge.size() < bridge_length) {
+
+                // 다리 입장 가능 총 중량 확인 조건
+                if (sumWeight + enterTruck <= weight) {
+
+                    bridge.add(enterTruck);
+
+                    sumWeight += enterTruck;
                 } else {
-                    int outTruck = bridge.poll();
-                    if (outTruck != 0) {
-                        sumWeight -= outTruck;
-                        seconds++;
-                    }
+                    // 지금은 다리에 무게가 꽉찬상태다. 
+                    // 무게가 7인 트럭 1개가 2칸의 다리를 건너야한다. 
+                    // 다리에서 내리기직전의 시간까지 구한다.
+                    seconds += bridge_length-bridge.size();
                 }
+
+            } else {
+                int outTruck = bridge.poll();
+                if (outTruck != 0) {
+                    sumWeight -= outTruck;
+                }
+            }
+
+            seconds++;
+
+            // 다리에 차가 아무것도 없을 경우 종료한다. 
+            if (bridge.size() == 0) {
+                break;
             }
 
         }
         
-        
+        // 트럭 대기 큐 하나쓰고 / 트럭 인 브릿지 큐하나 써서 
+        // 와일돌리고 둘다 사이즈 영이면 끝내는 각보자.
        
         
         
